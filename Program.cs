2fb07e7169423697f1fd5260c951846bdeffb098
NathanHuffTL <nathan.huff@templelogic.com>
2024-04-22 14:30:29 -0400

    Added multiply case statement

--- Program.cs
+++ Program.cs
@@ -18,16 +18,19 @@ class Program
             return;
         }
 
-        // Console.WriteLine("Enter the operation (+, -, *, /):");
-        // char operation = Console.ReadKey().KeyChar;
+        Console.WriteLine("Enter the operation (+, *):");
+        string operation = Console.ReadLine();
         Console.WriteLine(); // Move to the next line after reading the operation key
-        char operation = '+';
         double result;
         switch (operation)
         {
-            case '+':
+            case "+":
                 result = num1 + num2;
                 Console.WriteLine($"Result: {num1} + {num2} = {result}");
+                break;
+            case "*":
+                result = num1 * num2;
+                Console.WriteLine($"Result: {num1} * {num2} = {result}");
                 break;
             default:
                 Console.WriteLine("Invalid operation. Please enter a valid operation (+, -, *, /).");
